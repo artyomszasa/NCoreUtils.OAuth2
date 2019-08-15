91e7a87dd82e6b9e22c12ba57ebafba1498af261
Artyom Antyipin <artyom.szasa@gmail.com>
2019-08-15 12:47:12 +0200

    version bumps.

--- NCoreUtils.OAuth2.Client/Encoder.fs
+++ NCoreUtils.OAuth2.Client/Encoder.fs
@@ -1,10 +1,56 @@
 namespace NCoreUtils.OAuth2
 
 open System.Runtime.InteropServices
+open System.Threading.Tasks
+open NCoreUtils
 open NCoreUtils.AspNetCore.Authentication.OpenId
 open NCoreUtils.Authentication
-open NCoreUtils.Interop
 open FSharp.Control
+
+[<AutoOpen>]
+module private Adapter =
+
+  let ofAsyncEnumerable (enumerable : System.Collections.Generic.IAsyncEnumerable<_>) =
+    { new IAsyncEnumerable<_> with
+        member __.GetEnumerator () =
+          let enumerator = enumerable.GetAsyncEnumerator ()
+          { new IAsyncEnumerator<_> with
+              member __.MoveNext () = async {
+                let! next = Async.VAdapt (fun _ -> enumerator.MoveNextAsync())
+                return
+                  match next with
+                  | true -> Some enumerator.Current
+                  | _    -> None }
+              member __.Dispose () =
+                enumerator.DisposeAsync().AsTask().Wait()
+          }
+    }
+
+  let toAsyncEnumerable (enumerable : IAsyncEnumerable<_>) =
+    { new System.Collections.Generic.IAsyncEnumerable<_> with
+        member __.GetAsyncEnumerator cancellationToken =
+          let enumerator = enumerable.GetEnumerator ()
+          let current = ref Unchecked.defaultof<_>
+          { new System.Collections.Generic.IAsyncEnumerator<_> with
+              member __.Current = !current
+              member __.MoveNextAsync () =
+                let computation = async {
+                  let! next = enumerator.MoveNext ()
+                  return
+                    match next with
+                    | Some v ->
+                      current := v
+                      true
+                    | None ->
+                      current := Unchecked.defaultof<_>
+                      false }
+                Async.StartAsTask (computation, cancellationToken = cancellationToken)
+                |> ValueTask<bool>
+              member __.DisposeAsync () =
+                enumerator.Dispose ()
+                Unchecked.defaultof<_>
+          }
+    }
 
 type OpenIdWithScopesAuthenticationTicketEncoder<'TOpenIdClientConfiguration when 'TOpenIdClientConfiguration :> OpenIdClientConfiguration> =
   inherit OpenIdAuthenticationTicketEncoder<NCoreUtils.OAuth2.OpenIdUserInfo, 'TOpenIdClientConfiguration>
